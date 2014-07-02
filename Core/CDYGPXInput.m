a7caba41f08ea123b6b806c81d3e1cb0e540f09e
Jaanus Siim <jaanus@jaanussiim.com>
2014-07-02 17:05:31 +0300

    Basic data read and feed

---
+++ Core/CDYGPXInput.m
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2014 Coodly LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "CDYGPXInput.h"
+#import "ONOXMLDocument.h"
+#import "CDYLocation.h"
+
+@interface CDYGPXInput ()
+
+@property (nonatomic, strong) NSURL *inputFileURL;
+@property (nonatomic, strong) NSArray *locations;
+@property (nonatomic, assign) NSUInteger readLocation;
+
+@end
+
+@implementation CDYGPXInput
+
+- (instancetype)initWithFileURL:(NSURL *)inputFileURL {
+    self = [super init];
+    if (self) {
+        _inputFileURL = inputFileURL;
+    }
+    return self;
+}
+
+- (void)prepareInput:(CDYLocationActionBlock)completion {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        CDYLSLog(@"Load data from:%@", self.inputFileURL);
+        NSData *data = [NSData dataWithContentsOfURL:self.inputFileURL];
+
+        NSError *parseError = nil;
+        ONOXMLDocument *document = [ONOXMLDocument XMLDocumentWithData:data error:&parseError];
+        if (parseError) {
+            CDYLSLog(@"Parse error:%@", parseError);
+        }
+
+        NSMutableArray *locations = [NSMutableArray array];
+
+        NSArray *tracks = [document.rootElement childrenWithTag:@"trk"];
+        for (ONOXMLElement *trackElement in tracks) {
+            NSArray *segments = [trackElement childrenWithTag:@"trkseg"];
+            for (ONOXMLElement *segmentElement in segments) {
+                NSArray *points = [segmentElement childrenWithTag:@"trkpt"];
+                for (ONOXMLElement *point in points) {
+                    NSString *latitude = point.attributes[@"lat"];
+                    NSString *longitude = point.attributes[@"lon"];
+                    NSDate *date = [point firstChildWithTag:@"time"].dateValue;
+                    ONOXMLElement *extensions = [point firstChildWithTag:@"extensions"];
+                    ONOXMLElement *trackPointExtensions = [extensions firstChildWithTag:@"TrackPointExtension"];
+                    ONOXMLElement *speedElement = [trackPointExtensions firstChildWithTag:@"speed"];
+                    ONOXMLElement *courseElement = [trackPointExtensions firstChildWithTag:@"course"];
+                    NSNumber *speed = @(speedElement.stringValue.doubleValue);
+                    NSNumber *course = @(courseElement.stringValue.doubleValue);
+
+                    CDYLocation *location = [[CDYLocation alloc] initWithCoordinate:CLLocationCoordinate2DMake(latitude.doubleValue, longitude.doubleValue)
+                                                                           altitude:0
+                                                                 horizontalAccuracy:5
+                                                                   verticalAccuracy:0
+                                                                             course:course.doubleValue
+                                                                              speed:speed.doubleValue
+                                                                          timestamp:date];
+                    [locations addObject:location];
+                }
+            }
+        }
+
+        CDYLSLog(@"Did read %d locations", locations.count);
+        [self setLocations:[NSArray arrayWithArray:locations]];
+        dispatch_async(dispatch_get_main_queue(), completion);
+    });
+}
+
+- (BOOL)isReady {
+    return self.locations.count > 0;
+}
+
+- (CDYLocation *)nextLocation {
+    if (self.readLocation >= self.locations.count) {
+        [self setReadLocation:0];
+    }
+
+    CDYLocation *location = self.locations[self.readLocation];
+    self.readLocation++;
+    return location;
+}
+
+@end
---
+++ Core/CDYLocationManager.m
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014 Coodly LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "CDYLocationManager.h"
+#import "CDYLocationsInput.h"
+
+@interface CDYLocationManager ()
+
+@property (nonatomic, strong) CDYLocationsInput *locationsInput;
+@property (nonatomic, assign) BOOL started;
+@property (nonatomic, assign) BOOL pushLocations;
+
+@end
+
+@implementation CDYLocationManager
+
+- (instancetype)initWithInput:(CDYLocationsInput *)input {
+    self = [super init];
+    if (self) {
+        _locationsInput = input;
+    }
+    return self;
+}
+
+- (void)startUpdatingLocation {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        CDYLSLog(@"startUpdatingLocation");
+        [self setPushLocations:YES];
+
+        if (!self.started && ![self.locationsInput isReady]) {
+            [self.locationsInput prepareInput:^{
+                [self pushNextLocation];
+            }];
+        } else if ([self.locationsInput isReady]) {
+            [self pushNextLocation];
+        }
+
+        [self setStarted:YES];
+    });
+}
+
+- (void)stopUpdatingLocation {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self setPushLocations:NO];
+    });
+}
+
+- (void)pushNextLocation {
+    if (!self.pushLocations) {
+        CDYLSLog(@"Updates stopped");
+        return;
+    }
+
+    CDYLocation *location = [self.locationsInput nextLocation];
+    [self.delegate locationManager:self didUpdateLocations:@[location]];
+    CDYLocationDelayedExecution(1, ^{
+        [self pushNextLocation];
+    });
+}
+
+@end
